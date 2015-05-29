cd44cb00b10ffc5e055fb26c1b11534a4bdeb6c6
mdsimmo <mdsimmo2+git@gmail.com>
2015-05-29 14:43:38 +1000

    merged GridEliminator and RowEliminator

--- src/com/github/mdsimmo/sudokusolver/solvers/GridEliminator.java
+++ src/com/github/mdsimmo/sudokusolver/solvers/GridEliminator.java
@@ -9,23 +9,35 @@ public class GridEliminator implements Solver {
     @Override
     public boolean apply ( Board board ) {
         boolean infoFound = false;
-
-        int size = board.getSize();
+        final int size = board.size();
         for ( int i = 1; i <= size; i++ ) {
-            Grid grid = board.getSubGrid( i );
-            for ( int j = 1; j <= size; j++ ) {
-                Cell cell = grid.getCell( j );
-                if ( !cell.isSolved() )
-                    continue;
-                int value = cell.getSolution();
-                for ( int k = 1; k <= size; k++ ) {
-                    if ( k == j )
-                        continue;
-                    infoFound |= grid.getCell( k ).setImpossible( value );
-                }
-            }
+            infoFound |= apply( board.getSubGrid( i ) );
+            infoFound |= apply( board.getRow( i ) );
+            infoFound |= apply( board.getColumn( i ) );
         }
-
         return infoFound;
+    }
+
+    public boolean apply( Grid grid ) {
+        final int size = grid.size();
+        boolean foundInfo = false;
+        for ( int i = 1; i <= size; i++ ) {
+            Cell cell = grid.getCell( i );
+            if ( cell.isSolved() )
+                foundInfo |= eliminateAllFrom( grid, cell.getSolution() );
+        }
+        return foundInfo;
+    }
+
+    public boolean eliminateAllFrom( Grid grid, int number ) {
+        final int size = grid.size();
+        boolean foundInfo = false;
+        for ( int i = 1; i <= size; i++ ) {
+            Cell cell = grid.getCell( i );
+            if ( cell.isSolved() )
+                continue;
+            foundInfo |= cell.setImpossible( number );
+        }
+        return foundInfo;
     }
 }
--- tests/com/github/mdsimmo/sudokusolver/tests/solvers/GridEliminatorTest.java
+++ tests/com/github/mdsimmo/sudokusolver/tests/solvers/GridEliminatorTest.java
@@ -1,8 +1,9 @@
 package com.github.mdsimmo.sudokusolver.tests.solvers;
 
 import com.github.mdsimmo.sudokusolver.components.Board;
+import com.github.mdsimmo.sudokusolver.components.Grid;
 import com.github.mdsimmo.sudokusolver.solvers.GridEliminator;
-import com.github.mdsimmo.sudokusolver.solvers.RowEliminator;
+import com.github.mdsimmo.sudokusolver.solvers.Solver;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,17 +11,79 @@ import static org.junit.Assert.*;
 public class GridEliminatorTest {
 
     @Test
-    public void testApply() throws Exception {
+    public void testApplyGrid() throws Exception {
         Board board = new Board( 4 );
         board.getCell( 1, 1 ).setSolution( 1 );
         board.getCell( 1, 2 ).setSolution( 2 );
         board.getCell( 2, 1 ).setSolution( 3 );
 
-        board.getCell( 4, 4 ).setSolution( 4 );
-
-        new GridEliminator().apply( board );
+        assertTrue( new GridEliminator().apply( board ) );
 
         assertEquals( 4, board.getCell( 2, 2 ).getSolution() );
-        assertFalse( board.getCell( 3, 3 ).isPossible( 4 ) );
+    }
+
+    @Test
+    public void testApplyColumn() throws Exception {
+        Board board = new Board( 4 );
+        board.getCell( 3, 1 ).setSolution( 1 );
+        board.getCell( 3, 2 ).setSolution( 2 );
+        board.getCell( 3, 4 ).setSolution( 3 );
+
+        assertTrue( new GridEliminator().apply( board ) );
+
+        assertEquals( 4, board.getCell( 3, 3 ).getSolution() );
+
+    }
+
+    @Test
+    public void testApplyRow() throws Exception {
+        Board board = new Board( 4 );
+        board.getCell( 1, 3 ).setSolution( 1 );
+        board.getCell( 2, 3 ).setSolution( 2 );
+        board.getCell( 3, 3 ).setSolution( 3 );
+
+        assertTrue( new GridEliminator().apply( board ) );
+
+        assertEquals( 4, board.getCell( 4, 3 ).getSolution() );
+
+    }
+
+    @Test
+    public void testNotClaimingToSolve() {
+        Board board = new Board( 4 );
+        Solver solver = new GridEliminator();
+
+        assertFalse( new GridEliminator().apply( board ) );
+
+        board.getCell( 1, 1 ).setSolution( 1 );
+
+        assertTrue( solver.apply( board ) );
+        assertFalse( solver.apply( board ) );
+    }
+    @Test
+    public void testEliminateAll() {
+        Board board = new Board( 4 );
+        Grid grid = board.getSubGrid( 2 );
+
+        assertTrue( new GridEliminator().eliminateAllFrom( grid, 1 ) );
+
+        assertFalse( grid.getCell( 1 ).isPossible( 1 ) );
+        assertFalse( grid.getCell( 2 ).isPossible( 1 ) );
+        assertFalse( grid.getCell( 3 ).isPossible( 1 ) );
+        assertFalse( grid.getCell( 4 ).isPossible( 1 ) );
+
+        assertTrue( grid.getCell( 2 ).isPossible( 2 ) );
+        assertTrue( grid.getCell( 2 ).isPossible( 3 ) );
+        assertTrue( grid.getCell( 2 ).isPossible( 4 ) );
+    }
+
+    public void testEliminatedAround() {
+        Board board = new Board( 4 );
+        board.getCell( 3, 3 ).setSolution( 1 );
+        new GridEliminator().apply( board );
+
+        assertFalse( board.getCell( 4, 4 ).isPossible( 1 ) );
+        assertFalse( board.getCell( 1, 3 ).isPossible( 1 ) );
+        assertFalse( board.getCell( 3, 1 ).isPossible( 1 ) );
     }
 }
